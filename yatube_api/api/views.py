800c41f5bddb8e1ea9ef7f2a696a14cbcb54aaf1
Anastasiya Resnickaya <a.kostoyanskaya@yandex.ru>
2024-08-30 17:20:20 +0300

    fix

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import get_object_or_404
-from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, viewsets, permissions
 from rest_framework.pagination import LimitOffsetPagination
 
